e1010c4020945c1414c1ed33d1e7503a799aa887
Hugo Faria <hugofaria25500@gmail.com>
2026-04-11 18:12:38 +0100

    creating default pages

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+/*COMPONENTS*/
+import Navbar from "../components/Navbar";
+
+function LoginPage() {
+    return (
+        <div>
+            <Navbar />
+            <h1 className="mt-[100px]">Login Page</h1>
+        </div>
+    );
+}
+
+export default LoginPage;
