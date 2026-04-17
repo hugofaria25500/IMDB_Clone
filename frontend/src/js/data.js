ade46585fff3e35d3d324b2df92d4d128460439b
Hugo Faria <hugofaria25500@gmail.com>
2026-04-17 17:36:38 +0100

    organizing mock data

--- frontend/src/js/data.js
+++ frontend/src/js/data.js
@@ -1,374 +1,9 @@
-export const popularMovies = [
-  {
-    id: 1,
-    title: "Inception",
-    year: 2010,
-    rating: 8.8,
-    image: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"
-  },
-  {
-    id: 2,
-    title: "The Dark Knight",
-    year: 2008,
-    rating: 9.0,
-    image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
-  },
-  {
-    id: 3,
-    title: "Interstellar",
-    year: 2014,
-    rating: 8.6,
-    image: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg"
-  },
-  {
-    id: 4,
-    title: "Avengers: Endgame",
-    year: 2019,
-    rating: 8.4,
-    image: "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
-  },
-  {
-    id: 5,
-    title: "Gladiator",
-    year: 2000,
-    rating: 8.5,
-    image: "https://image.tmdb.org/t/p/w500/ty8TGRuvJLPUmAR1H1nRIsgwvim.jpg"
-  },
-  {
-    id: 6,
-    title: "Joker",
-    year: 2019,
-    rating: 8.4,
-    image: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"
-  },
-  {
-    id: 7,
-    title: "Titanic",
-    year: 1997,
-    rating: 7.9,
-    image: "https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg"
-  },
-  {
-    id: 8,
-    title: "Avatar",
-    year: 2009,
-    rating: 7.8,
-    image: "https://image.tmdb.org/t/p/w500/kyeqWdyUXW608qlYkRqosgbbJyK.jpg"
-  },
-  {
-    id: 9,
-    title: "The Matrix",
-    year: 1999,
-    rating: 8.7,
-    image: "https://image.tmdb.org/t/p/w500/aOIuZAjPaRIE6CMzbazvcHuHXDc.jpg"
-  },
-  {
-    id: 10,
-    title: "Fight Club",
-    year: 1999,
-    rating: 8.8,
-    image: "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
-  },
-  {
-    id: 11,
-    title: "Forrest Gump",
-    year: 1994,
-    rating: 8.8,
-    image: "https://image.tmdb.org/t/p/w500/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg"
-  },
-  {
-    id: 12,
-    title: "The Shawshank Redemption",
-    year: 1994,
-    rating: 9.3,
-    image: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"
-  },
-  {
-    id: 13,
-    title: "The Godfather",
-    year: 1972,
-    rating: 9.2,
-    image: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg"
-  },
-  {
-    id: 14,
-    title: "Pulp Fiction",
-    year: 1994,
-    rating: 8.9,
-    image: "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
-  },
-  {
-    id: 15,
-    title: "The Lion King",
-    year: 1994,
-    rating: 8.5,
-    image: "https://image.tmdb.org/t/p/w500/sKCr78MXSLixwmZ8DyJLrpMsd15.jpg"
-  },
-  {
-    id: 16,
-    title: "Spider-Man: No Way Home",
-    year: 2021,
-    rating: 8.2,
-    image: "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg"
-  },
-  {
-    id: 17,
-    title: "The Batman",
-    year: 2022,
-    rating: 7.8,
-    image: "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg"
-  },
-  {
-    id: 18,
-    title: "Dune",
-    year: 2021,
-    rating: 8.0,
-    image: "https://image.tmdb.org/t/p/w500/d5NXSklXo0qyIYkgV94XAgMIckC.jpg"
-  },
-  {
-    id: 19,
-    title: "John Wick",
-    year: 2014,
-    rating: 7.4,
-    image: "https://image.tmdb.org/t/p/w500/fZPSd91yGE9fCcCe6OoQr6E4x2y.jpg"
-  },
-  {
-    id: 20,
-    title: "Mad Max: Fury Road",
-    year: 2015,
-    rating: 8.1,
-    image: "https://image.tmdb.org/t/p/w500/8tZYtuWezp8JbcsvHYO0O46tFbo.jpg"
-  }
-];
+//////////////////////////////////////////////////////////////////////////////////
+// 
+//                             MOVIES MOCK DATA                                 //
+// 
+//////////////////////////////////////////////////////////////////////////////////  
 
-export const trendingMovies = [
-  {
-    id: 101,
-    title: "Dune: Part Two",
-    year: 2024,
-    rating: 8.7,
-    image: "https://image.tmdb.org/t/p/w500/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg"
-  },
-  {
-    id: 102,
-    title: "Oppenheimer",
-    year: 2023,
-    rating: 8.6,
-    image: "https://image.tmdb.org/t/p/w500/8Gxv9d6xk9J5z1YkRk9o6G5nJ6Q.jpg"
-  },
-  {
-    id: 103,
-    title: "Barbie",
-    year: 2023,
-    rating: 7.2,
-    image: "https://image.tmdb.org/t/p/w500/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg"
-  },
-  {
-    id: 104,
-    title: "The Marvels",
-    year: 2023,
-    rating: 6.5,
-    image: "https://image.tmdb.org/t/p/w500/Ag3D9qXjhJ2FUkrlJ0Cv1pgxqYQ.jpg"
-  },
-  {
-    id: 105,
-    title: "Wonka",
-    year: 2023,
-    rating: 7.0,
-    image: "https://image.tmdb.org/t/p/w500/qhb1qOilapbapxWQn9jtRCMwXJF.jpg"
-  },
-  {
-    id: 106,
-    title: "Napoleon",
-    year: 2023,
-    rating: 6.7,
-    image: "https://image.tmdb.org/t/p/w500/jE5o7y9K6pZtWNN6f9l0z5jY7cC.jpg"
-  },
-  {
-    id: 107,
-    title: "The Creator",
-    year: 2023,
-    rating: 7.1,
-    image: "https://image.tmdb.org/t/p/w500/vBZ0qvaRxqEhZwl6LWmruJqWE8Z.jpg"
-  },
-  {
-    id: 108,
-    title: "Aquaman 2",
-    year: 2023,
-    rating: 6.0,
-    image: "https://image.tmdb.org/t/p/w500/7lTnXOy0iNtBAdRP3TZvaKJ77F6.jpg"
-  },
-  {
-    id: 109,
-    title: "Killers of the Flower Moon",
-    year: 2023,
-    rating: 7.8,
-    image: "https://image.tmdb.org/t/p/w500/dB6Krk806zeqd0YNp2ngQ9zXteH.jpg"
-  },
-  {
-    id: 110,
-    title: "John Wick 4",
-    year: 2023,
-    rating: 7.9,
-    image: "https://image.tmdb.org/t/p/w500/vZloFAK7NmvMGKE7VkF5UHaz0I.jpg"
-  },
-  {
-    id: 111,
-    title: "Mission Impossible 7",
-    year: 2023,
-    rating: 7.7,
-    image: "https://image.tmdb.org/t/p/w500/NNxYkU70HPurnNCSiCjYAmacwm.jpg"
-  },
-  {
-    id: 112,
-    title: "The Flash",
-    year: 2023,
-    rating: 6.8,
-    image: "https://image.tmdb.org/t/p/w500/rktDFPbfHfUbArZ6OOOKsXcv0Bm.jpg"
-  },
-  {
-    id: 113,
-    title: "Blue Beetle",
-    year: 2023,
-    rating: 6.9,
-    image: "https://image.tmdb.org/t/p/w500/mXLOHHc1Zeuwsl4xYKjKh2280oL.jpg"
-  },
-  {
-    id: 114,
-    title: "Gran Turismo",
-    year: 2023,
-    rating: 7.1,
-    image: "https://image.tmdb.org/t/p/w500/51tqzRtKMMZEYUpSYkrUE7v9ehm.jpg"
-  },
-  {
-    id: 115,
-    title: "Elemental",
-    year: 2023,
-    rating: 7.0,
-    image: "https://image.tmdb.org/t/p/w500/6oH378KUfCEitzJkm07r97L0RsZ.jpg"
-  },
-  {
-    id: 116,
-    title: "Five Nights at Freddy's",
-    year: 2023,
-    rating: 6.5,
-    image: "https://image.tmdb.org/t/p/w500/7BpNtNfxuocYEVREzVMO75hso1l.jpg"
-  },
-  {
-    id: 117,
-    title: "The Hunger Games: Ballad",
-    year: 2023,
-    rating: 7.2,
-    image: "https://image.tmdb.org/t/p/w500/mBaXZ95R2OxueZhvQbcEWy2DqyO.jpg"
-  },
-  {
-    id: 118,
-    title: "Rebel Moon",
-    year: 2023,
-    rating: 6.2,
-    image: "https://image.tmdb.org/t/p/w500/ui4DrH1cKk2vkHshcUcGt2lKxCm.jpg"
-  },
-  {
-    id: 119,
-    title: "The Nun II",
-    year: 2023,
-    rating: 6.0,
-    image: "https://image.tmdb.org/t/p/w500/5gzzkR7y3hnY8AD1wXjCnVlHba5.jpg"
-  },
-  {
-    id: 120,
-    title: "Saw X",
-    year: 2023,
-    rating: 7.0,
-    image: "https://image.tmdb.org/t/p/w500/aQPeznSu7XDTrrdCtT5eLiu52Yu.jpg"
-  }
-];
-
-export const newMovieReleases = [
-  {
-    id: 693134,
-    title: "Dune: Part Two",
-    year: 2024,
-    rating: 8.7,
-    description: "Paul Atreides unites with the Fremen to seek revenge against those who destroyed his family while embracing his destiny.",
-    image: "https://image.tmdb.org/t/p/w1280/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg"
-  },
-  {
-    id: 9806,
-    title: "The Incredibles",
-    year: 2004,
-    rating: 7.7,
-    description: "A family of undercover superheroes is forced back into action to save the world from a dangerous new villain.",
-    image: "https://image.tmdb.org/t/p/w1280/se5Hxz7PArQZOG3Nx2bpfOhLhtV.jpg"
-  },
-  {
-    id: 76600,
-    title: "WALL-E",
-    year: 2008,
-    rating: 8.0,
-    description: "A small waste-collecting robot embarks on a space journey that will decide the fate of humanity.",
-    image: "https://image.tmdb.org/t/p/w1280/fK5ssgvtI43z19FoWigdlqgpLRE.jpg"
-  },
-  {
-    id: 19995,
-    title: "Avatar",
-    year: 2009,
-    rating: 7.5,
-    description: "A marine on an alien planet becomes torn between following orders and protecting the world he feels is his home.",
-    image: "https://image.tmdb.org/t/p/w1280/vL5LR6WdxWPjLPFRLe133jXWsh5.jpg"
-  },
-  {
-    id: 603,
-    title: "The Matrix",
-    year: 1999,
-    rating: 8.2,
-    description: "A hacker discovers the true nature of reality and his role in the war against its controllers.",
-    image: "https://image.tmdb.org/t/p/w1280/l4QHerTSbMI7qgvasqxP36pqjN6.jpg"
-  }
-];
-
-export const newSeriesReleases = [
-  {
-    id: 1399,
-    title: "Game of Thrones",
-    year: 2011,
-    rating: 8.4,
-    description: "Noble families fight for control of the Iron Throne while an ancient enemy returns from beyond the Wall.",
-    image: "https://image.tmdb.org/t/p/w1280/gwPSoYUHAKmdyVywgLpKKA4BjRr.jpg"
-  },
-  {
-    id: 66732,
-    title: "Stranger Things",
-    year: 2016,
-    rating: 8.6,
-    description: "A group of kids uncover supernatural mysteries and secret experiments in their small town.",
-    image: "https://image.tmdb.org/t/p/w1280/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg"
-  },
-  {
-    id: 82856,
-    title: "The Mandalorian",
-    year: 2019,
-    rating: 8.5,
-    description: "A lone bounty hunter travels the outer reaches of the galaxy, far from the authority of the New Republic.",
-    image: "https://image.tmdb.org/t/p/w1280/9ijMGlJKqcslswWUzTEwScm82Gs.jpg"
-  },
-  {
-    id: 1396,
-    title: "Breaking Bad",
-    year: 2008,
-    rating: 9.5,
-    description: "A chemistry teacher turned meth producer partners with a former student in a dangerous criminal world.",
-    image: "https://image.tmdb.org/t/p/w1280/tsRy63Mu5cu8etL1X7ZLyf7UP1M.jpg"
-  },
-  {
-    id: 2316,
-    title: "The Office",
-    year: 2005,
-    rating: 8.5,
-    description: "A mockumentary on a group of typical office workers dealing with everyday absurd situations.",
-    image: "https://image.tmdb.org/t/p/w1280/7DJKHzAi83BmQrWLrYYOqcoKfhR.jpg"
-  }
-];
 
 export const moviesMock = [
   { id: 1, title: "Inception", year: 2010, rating: 8.8, image: "https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg" },
@@ -418,6 +53,85 @@ export const moviesMock = [
   { id: 45, title: "John Wick", year: 2014, rating: 7.4, image: "https://image.tmdb.org/t/p/w500/5vHssUeVe25bMrof1HyaPyWgaP.jpg" },
 ];
 
+export const popularMovies = [
+  moviesMock[0],
+  moviesMock[1],
+  moviesMock[2],
+  moviesMock[3],
+  moviesMock[4],
+  moviesMock[5],
+  moviesMock[6], 
+  moviesMock[7],
+  moviesMock[8],
+  moviesMock[9]
+]
+
+export const trendingMovies = [
+  moviesMock[10],
+  moviesMock[11],
+  moviesMock[12],
+  moviesMock[13],
+  moviesMock[14],
+  moviesMock[15],
+  moviesMock[16], 
+  moviesMock[17],
+  moviesMock[18],
+  moviesMock[19]
+]
+
+
+export const newMovieReleases = [
+  {
+    id: 693134,
+    title: "Dune: Part Two",
+    year: 2024,
+    rating: 8.7,
+    description: "Paul Atreides unites with the Fremen to seek revenge against those who destroyed his family while embracing his destiny.",
+    image: "https://image.tmdb.org/t/p/w1280/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg"
+  },
+  {
+    id: 9806,
+    title: "The Incredibles",
+    year: 2004,
+    rating: 7.7,
+    description: "A family of undercover superheroes is forced back into action to save the world from a dangerous new villain.",
+    image: "https://image.tmdb.org/t/p/w1280/se5Hxz7PArQZOG3Nx2bpfOhLhtV.jpg"
+  },
+  {
+    id: 76600,
+    title: "WALL-E",
+    year: 2008,
+    rating: 8.0,
+    description: "A small waste-collecting robot embarks on a space journey that will decide the fate of humanity.",
+    image: "https://image.tmdb.org/t/p/w1280/fK5ssgvtI43z19FoWigdlqgpLRE.jpg"
+  },
+  {
+    id: 19995,
+    title: "Avatar",
+    year: 2009,
+    rating: 7.5,
+    description: "A marine on an alien planet becomes torn between following orders and protecting the world he feels is his home.",
+    image: "https://image.tmdb.org/t/p/w1280/vL5LR6WdxWPjLPFRLe133jXWsh5.jpg"
+  },
+  {
+    id: 603,
+    title: "The Matrix",
+    year: 1999,
+    rating: 8.2,
+    description: "A hacker discovers the true nature of reality and his role in the war against its controllers.",
+    image: "https://image.tmdb.org/t/p/w1280/l4QHerTSbMI7qgvasqxP36pqjN6.jpg"
+  }
+];
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+// 
+//                             SERIES MOCK DATA                                 //
+// 
+//////////////////////////////////////////////////////////////////////////////////  
+
+
 export const seriesMock = [
   { id: 101, title: "Breaking Bad", year: 2008, rating: 9.5, image: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg" },
   { id: 102, title: "Game of Thrones", year: 2011, rating: 9.2, image: "https://image.tmdb.org/t/p/w500/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg" },
@@ -429,4 +143,94 @@ export const seriesMock = [
   { id: 108, title: "The Witcher", year: 2019, rating: 8.2, image: "https://image.tmdb.org/t/p/w500/7vjaCdMw15FEbXyLQTVa04URsPm.jpg" },
   { id: 109, title: "Peaky Blinders", year: 2013, rating: 8.8, image: "https://image.tmdb.org/t/p/w500/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg" },
   { id: 110, title: "Loki", year: 2021, rating: 8.2, image: "https://image.tmdb.org/t/p/w500/voHUmluYmKyleFkTu3lOXQG702u.jpg" },
+  { id: 111, title: "Breaking Bad", year: 2008, rating: 9.5, image: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg" },
+  { id: 112, title: "Game of Thrones", year: 2011, rating: 9.2, image: "https://image.tmdb.org/t/p/w500/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg" },
+  { id: 113, title: "Stranger Things", year: 2016, rating: 8.7, image: "https://image.tmdb.org/t/p/w500/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg" },
+  { id: 114, title: "The Office", year: 2005, rating: 8.9, image: "https://image.tmdb.org/t/p/w500/qWnJzyZhyy74gjpSjIXWmuk0ifX.jpg" },
+  { id: 115, title: "Friends", year: 1994, rating: 8.9, image: "https://image.tmdb.org/t/p/w500/f496cm9enuEsZkSPzCwnTESEK5s.jpg" },
+  { id: 116, title: "The Mandalorian", year: 2019, rating: 8.8, image: "https://image.tmdb.org/t/p/w500/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg" },
+  { id: 117, title: "Dark", year: 2017, rating: 8.8, image: "https://image.tmdb.org/t/p/w500/5LoLkH9nC6u3s4Qh6w3w4z1yXyS.jpg" },
+  { id: 118, title: "The Witcher", year: 2019, rating: 8.2, image: "https://image.tmdb.org/t/p/w500/7vjaCdMw15FEbXyLQTVa04URsPm.jpg" },
+  { id: 119, title: "Peaky Blinders", year: 2013, rating: 8.8, image: "https://image.tmdb.org/t/p/w500/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg" },
+  { id: 120, title: "Loki", year: 2021, rating: 8.2, image: "https://image.tmdb.org/t/p/w500/voHUmluYmKyleFkTu3lOXQG702u.jpg" },
+  { id: 121, title: "Breaking Bad", year: 2008, rating: 9.5, image: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg" },
+  { id: 122, title: "Game of Thrones", year: 2011, rating: 9.2, image: "https://image.tmdb.org/t/p/w500/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg" },
+  { id: 123, title: "Stranger Things", year: 2016, rating: 8.7, image: "https://image.tmdb.org/t/p/w500/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg" },
+  { id: 124, title: "The Office", year: 2005, rating: 8.9, image: "https://image.tmdb.org/t/p/w500/qWnJzyZhyy74gjpSjIXWmuk0ifX.jpg" },
+  { id: 125, title: "Friends", year: 1994, rating: 8.9, image: "https://image.tmdb.org/t/p/w500/f496cm9enuEsZkSPzCwnTESEK5s.jpg" },
+  { id: 126, title: "The Mandalorian", year: 2019, rating: 8.8, image: "https://image.tmdb.org/t/p/w500/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg" },
+  { id: 127, title: "Dark", year: 2017, rating: 8.8, image: "https://image.tmdb.org/t/p/w500/5LoLkH9nC6u3s4Qh6w3w4z1yXyS.jpg" },
+  { id: 128, title: "The Witcher", year: 2019, rating: 8.2, image: "https://image.tmdb.org/t/p/w500/7vjaCdMw15FEbXyLQTVa04URsPm.jpg" },
+  { id: 129, title: "Peaky Blinders", year: 2013, rating: 8.8, image: "https://image.tmdb.org/t/p/w500/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg" },
+  { id: 130, title: "Loki", year: 2021, rating: 8.2, image: "https://image.tmdb.org/t/p/w500/voHUmluYmKyleFkTu3lOXQG702u.jpg" },
+];
+
+export const popularSeries = [
+  seriesMock[0],
+  seriesMock[1],
+  seriesMock[2],
+  seriesMock[3],
+  seriesMock[4],
+  seriesMock[5],
+  seriesMock[6], 
+  seriesMock[7],
+  seriesMock[8],
+  seriesMock[9]
+]
+
+export const trendingSeries = [
+  seriesMock[19],
+  seriesMock[18],
+  seriesMock[17],
+  seriesMock[16],
+  seriesMock[15],
+  seriesMock[14],
+  seriesMock[13], 
+  seriesMock[12],
+  seriesMock[11],
+  seriesMock[10]
+]
+
+
+export const newSeriesReleases = [
+  {
+    id: 1399,
+    title: "Game of Thrones",
+    year: 2011,
+    rating: 8.4,
+    description: "Noble families fight for control of the Iron Throne while an ancient enemy returns from beyond the Wall.",
+    image: "https://image.tmdb.org/t/p/w1280/gwPSoYUHAKmdyVywgLpKKA4BjRr.jpg"
+  },
+  {
+    id: 66732,
+    title: "Stranger Things",
+    year: 2016,
+    rating: 8.6,
+    description: "A group of kids uncover supernatural mysteries and secret experiments in their small town.",
+    image: "https://image.tmdb.org/t/p/w1280/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg"
+  },
+  {
+    id: 82856,
+    title: "The Mandalorian",
+    year: 2019,
+    rating: 8.5,
+    description: "A lone bounty hunter travels the outer reaches of the galaxy, far from the authority of the New Republic.",
+    image: "https://image.tmdb.org/t/p/w1280/9ijMGlJKqcslswWUzTEwScm82Gs.jpg"
+  },
+  {
+    id: 1396,
+    title: "Breaking Bad",
+    year: 2008,
+    rating: 9.5,
+    description: "A chemistry teacher turned meth producer partners with a former student in a dangerous criminal world.",
+    image: "https://image.tmdb.org/t/p/w1280/tsRy63Mu5cu8etL1X7ZLyf7UP1M.jpg"
+  },
+  {
+    id: 2316,
+    title: "The Office",
+    year: 2005,
+    rating: 8.5,
+    description: "A mockumentary on a group of typical office workers dealing with everyday absurd situations.",
+    image: "https://image.tmdb.org/t/p/w1280/7DJKHzAi83BmQrWLrYYOqcoKfhR.jpg"
+  }
 ];
