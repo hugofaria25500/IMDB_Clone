7436cfd16d69aa2ad7af0c9595426e1c314e12d9
Hugo Faria <hugofaria25500@gmail.com>
2026-04-21 23:04:10 +0100

    creating the respetive catalog service functions

--- frontend/src/services/catalogService.js
+++ frontend/src/services/catalogService.js
@@ -1,9 +1,46 @@
-import { moviesMock, seriesMock } from "../js/data";
+/*MOVIES*/
+import { moviesMock, popularMovies, trendingMovies, newMoviesReleases, randomMovie} from "../js/data";
+/*SERIES*/
+import { seriesMock, popularSeries, trendingSeries, newSeriesReleases, randomSerie} from "../js/data";
 
+/*MOVIES*/
 export async function getMovies() {
   return moviesMock;
 }
 
+export async function getPopularMovies() {
+  return popularMovies;
+}
+
+export async function getTrendingMovies() {
+  return trendingMovies;
+}
+
+export async function getNewMoviesReleases() {
+  return newMoviesReleases;
+}
+
+export async function getRandomMovie() {
+  return randomMovie;
+}
+
+/*SERIES*/
 export async function getSeries() {
   return seriesMock;
+}
+
+export async function getPopularSeries() {
+  return popularSeries;
+}
+
+export async function getTrendingSeries() {
+  return trendingSeries;
+}
+
+export async function getNewSeriesReleases() {
+  return newSeriesReleases;
+}
+
+export async function getRandomSerie() {
+  return randomSerie;
 }
