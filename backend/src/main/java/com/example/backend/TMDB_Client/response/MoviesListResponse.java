cd4faaebea5512984239112238fb8fc488e8ba65
Hugo Faria <hugofaria25500@gmail.com>
2026-08-01 17:03:36 +0100

    refactoring movie list response in order to be reusable

--- backend/src/main/java/com/example/backend/TMDB_Client/response/MoviesListResponse.java
+++ backend/src/main/java/com/example/backend/TMDB_Client/response/MoviesListResponse.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class PopularMoviesListResponse {
+public class MoviesListResponse {
 
     @JsonProperty("results")
     private List<BasicMovieDTO> results;
