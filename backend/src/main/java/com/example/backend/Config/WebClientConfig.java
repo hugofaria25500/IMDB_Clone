3d0a6c87f5179e1ada55640c004b8893471a40f8
Hugo Faria <hugofaria25500@gmail.com>
2026-08-04 00:00:33 +0100

    adding config to reconnect to webclient and retry api request

--- backend/src/main/java/com/example/backend/Config/WebClientConfig.java
+++ backend/src/main/java/com/example/backend/Config/WebClientConfig.java
@@ -3,13 +3,23 @@ package com.example.backend.Config;
 import com.example.backend.TMDB_Client.config.TMDBProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient(TMDBProperties properties) {
-        return WebClient.builder().baseUrl(properties.getBaseUrl()).build();
+        HttpClient httpClient = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(10));
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .baseUrl(properties.getBaseUrl())
+                .build();
     }
 }
