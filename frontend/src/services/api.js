9fff85618086dea544aeead3ab03f3498a8a24cc
Hugo Faria <hugofaria25500@gmail.com>
2026-08-09 03:20:26 +0100

    aplying the favourite feature for movies and series

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,15 @@ const api = axios.create({
     baseURL: "http://localhost:8080/api",
 });
 
+api.interceptors.request.use((config) => {
+
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+});
+
 export default api;
